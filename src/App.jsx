15f727ebdf60f153d3a79e2f7b7d43f0a13e75fe
Danny Greenberg <greenberg08@gmail.com>
2024-06-03 14:20:58 -0400

    Remove old code

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,6 @@
-import { useState } from "react";
 import "./App.css";
-import ROICalculator from "./components/ROICalculator.jsx";
 
 function App() {
-  function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
-
-  const [threeMonthReturnValue, setThreeMonthReturnValue] = useState("");
-  const [threeMonthReturnValueAnnualized, setThreeMonthReturnValueAnnualized] =
-    useState("");
-  const [annualReturnValue, setAnnualReturnValue] = useState("");
-
-  const handleReturnValueCalculation = (
-    calculatedThreeMonthReturnValue,
-    calculatedThreeMonthReturnValueAnnualized,
-    calculatedAnnualReturnValue
-  ) => {
-    setThreeMonthReturnValue(numberWithCommas(calculatedThreeMonthReturnValue));
-    setThreeMonthReturnValueAnnualized(
-      numberWithCommas(calculatedThreeMonthReturnValueAnnualized)
-    );
-    setAnnualReturnValue(numberWithCommas(calculatedAnnualReturnValue));
-  };
-
   return (
     <>
       <header className="App-header">
@@ -42,17 +19,7 @@ function App() {
                 Begin by selecting your course of interest, your company’s
                 industry, and the number of employees you wish to enroll.
               </p>
-              {/* <ROICalculator onCalculate={handleReturnValueCalculation} />
-          {threeMonthReturnValue && (
-            <div>
-              <h2>Calculated Return (3 months): ${threeMonthReturnValue}</h2>
-              <h2>
-                Calculated Return (3 months – Annualized): $
-                {threeMonthReturnValueAnnualized}
-              </h2>
-              <h2>Calculated Return (1 year): ${annualReturnValue}</h2>
-            </div>
-          )} */}
+
               <form>
                 <div className="form-row">
                   <div className="form-element">
