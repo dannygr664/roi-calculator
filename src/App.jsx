f7169804ed55733fad2196dd177bc160f164122d
Danny Greenberg <greenberg08@gmail.com>
2024-06-03 11:56:23 -0400

    More restyling

--- src/App.jsx
+++ src/App.jsx
@@ -30,17 +30,18 @@ function App() {
         <img id="logo" src="images/zschool-logo.png" alt="Zschool logo"></img>
       </header>
       <main>
+        <h1>ROI Calculator</h1>
         <section className="intro">
-          <h1>ROI Calculator</h1>
-          <p>
-            This tool will calculate your estimated return on investment
-            following Zschool training, using various metrics.
-          </p>
-          <p>
-            Begin by selecting your course of interest, your company’s industry,
-            and the number of employees you wish to enroll.
-          </p>
-          {/* <ROICalculator onCalculate={handleReturnValueCalculation} />
+          <section className="intro-and-form">
+            <p>
+              This tool will calculate your estimated return on investment
+              following Zschool training, using various metrics.
+            </p>
+            <p>
+              Begin by selecting your course of interest, your company’s
+              industry, and the number of employees you wish to enroll.
+            </p>
+            {/* <ROICalculator onCalculate={handleReturnValueCalculation} />
           {threeMonthReturnValue && (
             <div>
               <h2>Calculated Return (3 months): ${threeMonthReturnValue}</h2>
@@ -51,34 +52,47 @@ function App() {
               <h2>Calculated Return (1 year): ${annualReturnValue}</h2>
             </div>
           )} */}
-          <form>
-            <div className="form-row">
-              <div className="form-element">
-                <label htmlFor="course">Course</label>
-                <select id="course" name="course">
-                  <option value="">Select</option>
-                </select>
+            <form>
+              <div className="form-row">
+                <div className="form-element">
+                  <label htmlFor="course">Course</label>
+                  <div className="custom-select">
+                    <select id="course" name="course">
+                      <option value="">Select</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div className="form-element">
+                  <label htmlFor="industry">Industry</label>
+                  <div className="custom-select">
+                    <select id="industry" name="industry">
+                      <option value="">Select</option>
+                    </select>
+                  </div>
+                </div>
               </div>
 
               <div className="form-element">
-                <label htmlFor="industry">Industry</label>
-                <select id="industry" name="industry">
-                  <option value="">Select</option>
-                </select>
+                <label htmlFor="employees">Number of Employees</label>
+                <input
+                  type="number"
+                  id="employees"
+                  name="employees"
+                  value="0"
+                />
               </div>
-            </div>
+            </form>
+          </section>
 
-            <label htmlFor="employees">Number of Employees</label>
-            <input type="number" id="employees" name="employees" value="0" />
-          </form>
+          <aside>
+            <img
+              src="images/roi-intro.jpg"
+              alt="ROI Calculator Intro"
+              className="intro-image"
+            />
+          </aside>
         </section>
-        <aside>
-          <img
-            src="images/roi-intro.jpg"
-            alt="ROI Calculator Intro"
-            className="intro-image"
-          />
-        </aside>
       </main>
     </>
   );
