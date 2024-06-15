1920f7943bf9b8c4c1fcca605eb40701ddc9673c
Danny Greenberg <greenberg08@gmail.com>
2024-06-14 20:12:37 -0400

    Add remaining Learning Survey questions

--- src/surveys/constants.js
+++ src/surveys/constants.js
@@ -5,3 +5,12 @@ export const STANDARD_ANSWERS = [
   "Agree",
   "Strongly agree",
 ];
+
+export const PERCENTAGE_ANSWERS = [
+  "0–10%",
+  "11–20%",
+  "21–30%",
+  "31–40%",
+  "41–50%",
+  "More than 50%",
+];
--- src/surveys/learningSurvey.js
+++ src/surveys/learningSurvey.js
@@ -1,4 +1,4 @@
-import { STANDARD_ANSWERS } from "./constants";
+import { STANDARD_ANSWERS, PERCENTAGE_ANSWERS } from "./constants";
 
 const TITLE = "Post Learning Professional Development Survey";
 
@@ -8,6 +8,98 @@ const INSTRUCTIONS =
 const QUESTIONS_AND_ANSWERS = [
   {
     question: "The training objectives were clear and well-defined.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question: "The content of the training was relevant to my job.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question:
+      "The trainer was knowledgeable and effectively communicated the material.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question:
+      "The training materials (e.g., handouts, slides) were helpful and well-prepared.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question: "The training included practical examples and exercises.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question:
+      "The training session was interactive and encouraged participation.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question:
+      "I feel more confident in my abilities after completing this training.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question: "The training has positively impacted my job performance.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question:
+      "I have been able to apply the skills and knowledge learned in the training to my job.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question:
+      "Overall, I am satisfied with the investment the company made in my professional development.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question:
+      "The assignments provided during the training were relevant and beneficial to my job role.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question:
+      "The training has equipped me with industry knowledge that is essential for my role.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question:
+      "The training has improved my ability to handle job-specific tasks.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question:
+      "The assignments have enhanced my problem-solving skills related to my job.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question:
+      "The training has helped me to better understand the latest trends and developments in our industry.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question:
+      " I believe the training will contribute to improving the company’s ROI.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question:
+      "Approximately what percentage do you feel this course can help improve the company’s ROI?",
+    answers: PERCENTAGE_ANSWERS,
+  },
+  {
+    question:
+      "The training has provided me with new strategies to increase my productivity.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question:
+      "I feel better equipped to contribute to my team's success after this training.",
+    answers: STANDARD_ANSWERS,
+  },
+  {
+    question:
+      "The professional development training met my overall expectations.",
     answers: STANDARD_ANSWERS,
   },
 ];
