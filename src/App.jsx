f071d97deb2791e4b270f227b243a5ec4572572a
Danny Greenberg <greenberg08@gmail.com>
2024-06-18 13:48:32 -0400

    Add surveys and show survey corresponding with dropdown selection

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import ROICalculationPanel from "./components/ROICalculationPanel";
 import ROICalculationSurvey from "./components/ROICalculationSurvey";
 import "./App.css";
 import { LEARNING_SURVEY } from "./surveys/learningSurvey";
+import { TRAINEE_SATISFACTION_SURVEY } from "./surveys/traineeSatisfactionSurvey";
+import { WORKPLACE_BEHAVIOR_SURVEY } from "./surveys/workplaceBehaviorSurvey";
 
 function App() {
   const [trainingCosts, setTrainingCosts] = useState("0");
@@ -30,7 +32,7 @@ function App() {
             setSelectedRoiMetric={setSelectedRoiMetric}
           />
         )}
-        {isTrainingCostsCalculated && selectedRoiMetric && (
+        {isTrainingCostsCalculated && selectedRoiMetric === "learning" && (
           <ROICalculationSurvey
             title={LEARNING_SURVEY.title}
             instructions={LEARNING_SURVEY.instructions}
@@ -41,6 +43,36 @@ function App() {
             hrRecommendations={LEARNING_SURVEY.hrRecommendations}
           />
         )}
+        {isTrainingCostsCalculated &&
+          selectedRoiMetric === "traineeSatisfaction" && (
+            <ROICalculationSurvey
+              title={TRAINEE_SATISFACTION_SURVEY.title}
+              instructions={TRAINEE_SATISFACTION_SURVEY.instructions}
+              questionsAndAnswers={
+                TRAINEE_SATISFACTION_SURVEY.questionsAndAnswers
+              }
+              trainingCosts={trainingCosts}
+              resultDescriptions={
+                TRAINEE_SATISFACTION_SURVEY.resultDescriptions
+              }
+              feedbackIntros={TRAINEE_SATISFACTION_SURVEY.feedbackIntros}
+              hrRecommendations={TRAINEE_SATISFACTION_SURVEY.hrRecommendations}
+            />
+          )}
+        {isTrainingCostsCalculated &&
+          selectedRoiMetric === "workplaceBehavior" && (
+            <ROICalculationSurvey
+              title={WORKPLACE_BEHAVIOR_SURVEY.title}
+              instructions={WORKPLACE_BEHAVIOR_SURVEY.instructions}
+              questionsAndAnswers={
+                WORKPLACE_BEHAVIOR_SURVEY.questionsAndAnswers
+              }
+              trainingCosts={trainingCosts}
+              resultDescriptions={WORKPLACE_BEHAVIOR_SURVEY.resultDescriptions}
+              feedbackIntros={WORKPLACE_BEHAVIOR_SURVEY.feedbackIntros}
+              hrRecommendations={WORKPLACE_BEHAVIOR_SURVEY.hrRecommendations}
+            />
+          )}
       </main>
     </>
   );
