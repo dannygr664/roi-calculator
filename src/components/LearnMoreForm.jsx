e4a49a01f75542c6fe37f98346b6366494a319bf
Danny Greenberg <greenberg08@gmail.com>
2024-06-24 14:41:14 -0400

    Use Formik in LearnMoreForm

--- src/components/LearnMoreForm.jsx
+++ src/components/LearnMoreForm.jsx
@@ -1,73 +1,74 @@
 import { useState } from "react";
+import { useFormik } from "formik";
 import PropTypes from "prop-types";
-
-import { areInputsValid } from "../utilities";
 
 import ErrorMessage from "./ErrorMessage";
 
 import "./LearnMoreForm.css";
 
 function LearnMoreForm({ surveyId }) {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
   const [isSubmitConfirmationVisible, setIsSubmitConfirmationVisible] =
     useState(false);
 
-  const [errors, setErrors] = useState({});
+  const validate = (values) => {
+    const errors = {};
 
-  const getValidationErrors = () => {
-    const newErrors = {};
-
-    if (!name) newErrors.name = "Please enter your name";
-    if (!email) newErrors.email = "Please enter your email";
-
-    return newErrors;
-  };
-
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (!areInputsValid(getValidationErrors, setErrors)) {
-      setIsSubmitConfirmationVisible(false);
-      return;
+    if (!values.name) {
+      errors.name = "Required";
     }
 
-    console.log(name, email);
-    setIsSubmitConfirmationVisible(true);
+    if (!values.email) {
+      errors.email = "Required";
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = "Invalid email address";
+    }
+
+    return errors;
   };
 
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+    },
+    validate,
+    onSubmit: (event, values) => {
+      console.log(values.name, values.email);
+      setIsSubmitConfirmationVisible(true);
+    },
+  });
+
   return (
-    <form className="submit-name-and-email-form" onSubmit={handleSubmit}>
+    <form className="submit-name-and-email-form" onSubmit={formik.handleSubmit}>
       <div className="submit-name-and-email-container">
         <div className="form-element">
-          <label htmlFor={`name-${surveyId}`}>Name</label>
+          <label htmlFor={`survey${surveyId}-name`}>Name</label>
           <input
             id="name"
             name="name"
             type="text"
-            value={name}
-            onChange={handleNameChange}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
           />
-          <ErrorMessage message={errors.name} />
+          {formik.touched.name && <ErrorMessage message={formik.errors.name} />}
         </div>
 
         <div className="form-element">
-          <label htmlFor={`email-${surveyId}`}>Email</label>
+          <label htmlFor={`survey${surveyId}-email`}>Email</label>
           <input
             id="email"
             name="email"
             type="email"
-            value={email}
-            onChange={handleEmailChange}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
           />
-          <ErrorMessage message={errors.email} />
+          {formik.touched.email && (
+            <ErrorMessage message={formik.errors.email} />
+          )}
         </div>
       </div>
 
